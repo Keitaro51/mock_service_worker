b4e291ac9c9f348d9dea93ea58c2edf326d67f51
R.Charlot <romain.charlot@lilo.org>
2023-02-20 10:35:20 +0100

    msw basics initiation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {useEffect, useState} from 'react'
+import axios from 'axios'
+
+const fetchUser = async () => {
+  const res = await axios.get('http://localhost:8000/api/user')
+  const data = await res.data
+  return data
+}
 
 function App() {
+  const [user, setUser] = useState('')
+  useEffect(() => {
+    fetchUser().then((data) => {
+      setUser(data.firstName)
+    })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{user}</p>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
